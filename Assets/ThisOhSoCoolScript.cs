f58720484b7c28565c55edd2c30913e133d025ee
francoislg <francois_lg@hotmail.com>
2014-09-18 19:35:37 -0400

    TestScript

---
+++ Assets/ThisOhSoCoolScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThisOhSoCoolScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = new Color (255, 255, 0); // Much Yellow
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
