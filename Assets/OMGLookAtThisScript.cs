cb49925d6bd45490057b6767369a5c240a5eec0b
francoislg <francois_lg@hotmail.com>
2014-09-18 20:12:11 -0400

    Commit avec du stuff qui marche, je pense.

---
+++ Assets/OMGLookAtThisScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OMGLookAtThisScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = new Color (255, 255, 0); // Much yellow
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
