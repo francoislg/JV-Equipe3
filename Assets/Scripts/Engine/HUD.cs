e7793a5c571aecdc85f305107fd6c420a1edd8de
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-10-09 01:57:10 -0400

    Refactor du HUD

--- Assets/Scripts/Engine/HUD.cs
+++ Assets/Scripts/Engine/HUD.cs
@@ -3,22 +3,44 @@ using System.Collections;
 
 public class HUD : MonoBehaviour
 {
-    public Rect pauseWindow;
     public Texture2D weaponIcon;
     public Texture2D powerUpIcon;
     public Texture2D healthIcon;
 
+    private const int ICON_SIZE = 70;
+    private const int SCREEN_MARGIN = 20;
+    private Rect pauseWindow;
+    private Rect powerUpPosition;
+    private Rect weaponPosition;
+    private Rect healthPosition;
+
     void Start()
     {
-        InitPauseWindow();
         InitSprites();
+        InitAttributes();
     }
 
-    void InitPauseWindow()
+    void InitAttributes()
     {
         int pauseW = 250;
         int pauseH = 100;
         pauseWindow = new Rect(Screen.width / 2 - pauseW / 2, Screen.height / 2 - pauseH / 2, pauseW, pauseH);
+
+        weaponPosition = new Rect(
+            SCREEN_MARGIN,
+            Screen.height - ICON_SIZE - SCREEN_MARGIN,
+            ICON_SIZE,
+            ICON_SIZE);
+        powerUpPosition = new Rect(
+            SCREEN_MARGIN + ICON_SIZE + 20,
+            Screen.height - ICON_SIZE - SCREEN_MARGIN, 
+            ICON_SIZE, 
+            ICON_SIZE);
+        healthPosition = new Rect(
+            Screen.width - ICON_SIZE - SCREEN_MARGIN,
+            Screen.height - ICON_SIZE - SCREEN_MARGIN, 
+            ICON_SIZE, 
+            ICON_SIZE);
     }
 
     void InitSprites()
@@ -41,12 +63,9 @@ public class HUD : MonoBehaviour
     {
         if (Time.timeScale > 0)
         {
-            GUI.Box(new Rect(20, 20, 70, 70), "Level 1");
-            GUI.Button(new Rect(30, 40, 50, 40), "Quit..."); //inside case 1
-
-            GUI.Box(new Rect(Screen.width - 90, 20, 70, 70), weaponIcon);
-            GUI.Box(new Rect(20, Screen.height - 90, 70, 70), powerUpIcon);
-            GUI.Box(new Rect(Screen.width - 90, Screen.height - 90, 70, 70), healthIcon);
+            GUI.Box(weaponPosition, weaponIcon);
+            GUI.Box(powerUpPosition, powerUpIcon);
+            GUI.Box(healthPosition, healthIcon);
         }
         else
         {
