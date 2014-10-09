03655dc78174098b8b3e129634c039d0a83016ba
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-10-08 22:35:39 -0400

    Suppresion du call au particle system pour les ne

--- Assets/Scripts/Enemies/EnemyMoving.cs
+++ Assets/Scripts/Enemies/EnemyMoving.cs
@@ -3,25 +3,18 @@ using System.Collections;
 
 public class EnemyMoving : MonoBehaviour
 {
+    public float speed = 1.0f;
+
     private GameObject target;
-    private float speed = 1.0f;
-    private ParticleSystem ps;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
-        ps = GetComponent<ParticleSystem>();
     }
 
     void Update()
     {
         transform.LookAt(target.transform.position);
         transform.position += transform.forward * speed * Time.deltaTime;
-    }
-
-    void onDestroy()
-    {
-        ps.Play();
-        ps.Stop();
     }
 }
