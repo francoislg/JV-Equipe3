67dbdcc219d0cb94e86be3cecf08e173e0db53a6
ftaleb <f_taleb@videotron.ca>
2014-10-25 18:47:27 -0400

    Scene GenTerrain pour le terrain fonctionnel

---
+++ Assets/Scripts/Engine/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    float camRayLength = 100f;
+    Rigidbody playerRigidbody;
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate () {
+        Move();
+        Turn();
+	}
+
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidbody.velocity = movement * speed;
+    }
+    void Turn()
+    {
+        //rayon de la souris à l'écran en direction de la camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+        RaycastHit floorHit;
+
+        // si le raycast atteint qqchose
+        if (Physics.Raycast(camRay, out floorHit, camRayLength))
+        {
+            // vecteur de player au point du plancher frappé par le raycast
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // rester en y
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+}
