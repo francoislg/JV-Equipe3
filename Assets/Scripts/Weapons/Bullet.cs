3ae3e24dab82c17cd857ffcc427635c3f0630c2b
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-10-25 20:26:44 -0400

    Les bullet ne touche plus le joueur

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -5,6 +5,19 @@ public class Bullet : MonoBehaviour
 {
     public int baseDamage = 10;
 
+    void Start()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject p in players)
+        {
+            Collider pc = p.GetComponent<Collider>();
+            if (pc)
+            {
+                Physics.IgnoreCollision(pc, collider);
+            }
+        }
+    }
+
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Spawner")
