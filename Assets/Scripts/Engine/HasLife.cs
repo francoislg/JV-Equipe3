28b8282722bdf62906013b9689000f5f877c04d5
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-10-25 20:57:56 -0400

    Déplacement du script HasLife

---
+++ Assets/Scripts/Engine/HasLife.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class HasLife : MonoBehaviour
+{
+    public float life = 10;
+
+    public virtual void ReceiveDamage(float damage)
+    {
+        life -= damage;
+        if (life <= 0)
+        {
+            OnDeath();
+        }
+    }
+
+    public virtual void PushFromSource(Vector3 source, float force)
+    {
+        Vector3 direction = transform.position - source;
+        direction.Normalize();
+        rigidbody.AddForce(direction * force);
+    }
+
+    public abstract void OnDeath();
+
+}
