65d66e037e3aad9598bf5e2568d4e4d6a46f3662
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-10-25 20:58:28 -0400

    Création de HasLife pour les enemis

---
+++ Assets/Scripts/Enemies/EnemyHasLife.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHasLife : HasLife
+{
+    public GameObject deathExplosion;
+
+    public override void OnDeath()
+    {
+        if (deathExplosion)
+        {
+            Instantiate(deathExplosion, gameObject.transform.position, Quaternion.Euler(90, 0, 0));
+        }
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Enemies/ZombiHasLife.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombiHasLife : HasLife
+{
+    public GameObject deathExplosion;
+
+    private ZombiAnimator animator;
+
+    void Start()
+    {
+        animator = GetComponent(typeof(ZombiAnimator)) as ZombiAnimator;
+    }
+
+    public override void OnDeath()
+    {
+        if (deathExplosion)
+        {
+            Instantiate(deathExplosion, gameObject.transform.position, Quaternion.Euler(90, 0, 0));
+        }
+        animator.state = ZombiAnimator.State.Dying;
+    }
+
+}
