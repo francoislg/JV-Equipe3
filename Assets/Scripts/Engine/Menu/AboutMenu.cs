a24f7823c0bfa3e04b0f889ffbe319d9b0b9876f
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-10-25 23:13:16 -0400

    Création d'un dossier pour les scripts des Menus

---
+++ Assets/Scripts/Engine/Menu/AboutMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AboutMenu : MonoBehaviour {
+
+    private const int buttonWidth = 200;
+    private const int buttonHeight = 60;
+    private const int margin = 10;
+
+    private Rect backButton;
+
+    private ScreenFader fader;
+
+    void Start()
+    {
+        fader = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
+
+        backButton = new Rect(
+            (Screen.width / 2) - (buttonWidth / 2),
+            Screen.height - buttonHeight - margin,
+            buttonWidth,
+            buttonHeight
+        );
+    }
+
+    void OnGUI()
+    {
+        if (!fader.SceneEnding && !fader.SceneStarting)
+        {
+            if (GUI.Button(backButton, "Retour au menu principal"))
+            {
+                fader.destinationSceneName = "MenuScene";
+                fader.EndCurrentScene();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Engine/Menu/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour
+{
+    private const int buttonWidth = 150;
+    private const int buttonHeight = 60;
+    private const int margin = 10;
+
+    private Rect playButton;
+    private Rect helpButton;
+
+    private ScreenFader fader;
+
+    void Start()
+    {
+        fader = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
+
+        playButton = new Rect(
+            20,
+            (Screen.height / 3) - (buttonHeight / 2),
+            buttonWidth,
+            buttonHeight
+        );
+
+        helpButton = new Rect(
+            playButton.xMin,
+            playButton.yMax + margin,
+            buttonWidth,
+            buttonHeight
+        );
+    }
+
+    void OnGUI()
+    {
+        if (!fader.SceneEnding && !fader.SceneStarting)
+        {
+            if (GUI.Button(playButton, "Jouer !"))
+            {
+                fader.destinationSceneName = "GameScene";
+                fader.EndCurrentScene();
+            }
+            else if (GUI.Button(helpButton, "A propos ..."))
+            {
+                fader.destinationSceneName = "AboutScene";
+                fader.EndCurrentScene();
+            }
+        }
+    }
+}
