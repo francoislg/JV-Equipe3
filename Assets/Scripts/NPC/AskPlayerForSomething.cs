41de923463ea6a1b2a7ec0b9f8e03a67b73e5322
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-11-09 18:39:17 -0500

    Ajout de script pour les NPC
    * Les NPC regardent le player
    * Il peuvent lui dire des phrases aléatoires
    * Ils peuvent demander une requète (min quest)

---
+++ Assets/Scripts/NPC/AskPlayerForSomething.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AskPlayerForSomething : TalkToPlayer
+{
+    protected override string GenerateMessage()
+    {
+        return "J'AI PERDU MON PORTE FEUILLE ... " +
+            "SI TU ME LE RETROUVE JE TE DONNERAI UNE BELLE RECOMPENSE !";
+    }
+}
---
+++ Assets/Scripts/NPC/SayRandomTalkToPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SayRandomTalkToPlayer : TalkToPlayer
+{
+    protected override string GenerateMessage()
+    {
+        string[] messages = {   
+            "SALUT !", 
+            "COMMENT CA VA ?", 
+            "IL FAIT ASSEZ BEAU AUJOURD HUI ...",
+            "ON SE CONNAIT ?", 
+            "CA SE VOIT QUE JE SUIS OCCUPE NON ?",
+            "JE T AI DEJA VU NON ?",
+            "BONJOUR !",
+            "BELLE JOURNEE, NON ?"
+        };
+        return messages[Random.Range(0, messages.Length)];
+    }
+}
