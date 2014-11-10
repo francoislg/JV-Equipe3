6c4f3e30896c37dc94858e6489a2e134c832e508
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-11-09 20:48:08 -0500

    Les NPC de quest peuvent donner des bonus

--- Assets/Scripts/NPC/Messaging/AskPlayerForSomething.cs
+++ Assets/Scripts/NPC/Messaging/AskPlayerForSomething.cs
@@ -4,18 +4,26 @@ using System.Collections;
 public class AskPlayerForSomething : TalkToPlayer
 {
     public string requestObject;
+
     public string questMessage;
     public string firstThanksMessage;
     public string thanksMessage;
 
+    public string giveWeaponName = "";
+    public int giveLifeBonus = 0;
+
+    PlayerHasLife playerLife;
     PlayerHasInventory playerInventory;
+    PlayerHasWeapon playerWeapons;
 
     bool questCompleted = false;
 
     protected override void Start()
     {
         base.Start();
+        playerLife = GameObject.FindObjectOfType<PlayerHasLife>();
         playerInventory = GameObject.FindObjectOfType<PlayerHasInventory>();
+        playerWeapons = GameObject.FindObjectOfType<PlayerHasWeapon>();
     }
 
     protected override string InteractWithPlayer()
@@ -26,13 +34,27 @@ public class AskPlayerForSomething : TalkToPlayer
         }
         else if (playerInventory.HaveQuestObject(requestObject))
         {
-            questCompleted = true;
-            playerInventory.RemoveQuestObject(requestObject);
+            OnQuestCompleted();
             return firstThanksMessage;
         }
         else
         {
             return questMessage;
+        }
+    }
+
+    void OnQuestCompleted()
+    {
+        questCompleted = true;
+        playerInventory.RemoveQuestObject(requestObject);
+
+        if (giveWeaponName.Length > 0)
+        {
+            playerWeapons.GiveWeapon(giveWeaponName);
+        }
+        if(giveLifeBonus > 0)
+        {
+            playerLife.ReceiveBonus(giveLifeBonus);
         }
     }
 
