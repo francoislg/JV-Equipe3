dd0d3afdbe41576c55e868bc40ff52988b725477
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-11-09 19:47:16 -0500

    Ajout d'un inventaire au joueur

---
+++ Assets/Scripts/Player/PlayerHasInventory.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerHasInventory : MonoBehaviour
+{
+    Dictionary<string, int> inventory = new Dictionary<string, int>();
+
+    public void AddQuestObject(string questObjectName)
+    {
+        if (!inventory.ContainsKey(questObjectName))
+        {
+            inventory[questObjectName] = 0;
+        }
+        inventory[questObjectName]++;
+    }
+
+    public void RemoveQuestObject(string questObjectName)
+    {
+        if (inventory.ContainsKey(questObjectName) ||
+            inventory[questObjectName] > 0)
+        {
+            inventory[questObjectName]--;
+        }
+    }
+
+    public bool HaveQuestObject(string questObjectName)
+    {
+        return inventory.ContainsKey(questObjectName) &&
+               inventory[questObjectName] > 0;
+    }
+}
