d4398f09cdb527e5f16dab3a10948dceafbb68b4
Alex Couët <couet12@gmail.com>
2014-11-13 12:49:39 -0500

    ajouter les explosions

--- Assets/Scripts/Enemies/EnemyHasChildren.cs
+++ Assets/Scripts/Enemies/EnemyHasChildren.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class EnemyHasChildren : HasLife
 {
 	public string childrenType;
+	public GameObject deathExplosion;
 
     public override void OnDeath()
     {
 		GameObject[] go = GameObject.FindGameObjectsWithTag(childrenType);
 		for(int i=0; i<go.Length; i++)
         {
-			Debug.Log("kill : " + childrenType);
+			Instantiate(deathExplosion, go[i].transform.position, Quaternion.Euler(90, 0, 0));
 			Destroy(go[i]);
         }
+		Instantiate(deathExplosion, gameObject.transform.position, Quaternion.Euler(90, 0, 0));
         Destroy(gameObject);
         
     }
