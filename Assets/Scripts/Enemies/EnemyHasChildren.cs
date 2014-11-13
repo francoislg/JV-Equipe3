180e8ffd8e31f3fec05fade197adee2290021da7
Alex Couët <couet12@gmail.com>
2014-11-13 12:01:37 -0500

    avec une helibase !

---
+++ Assets/Scripts/Enemies/EnemyHasChildren.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHasChildren : HasLife
+{
+	public string childrenType;
+
+    public override void OnDeath()
+    {
+		GameObject[] go = GameObject.FindGameObjectsWithTag(childrenType);
+		for(int i=0; i<go.Length; i++)
+        {
+			Debug.Log("kill : " + childrenType);
+			Destroy(go[i]);
+        }
+        Destroy(gameObject);
+        
+    }
+
+}
+
