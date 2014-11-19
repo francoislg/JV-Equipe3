34a0e4075cc0db1a46bf81475789c5798f2fe0ee
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-11-18 19:59:41 -0500

    Refactoring du GuiConfigurator

---
+++ Assets/Scripts/Engine/Skin/GuiConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiConfigurator : MonoBehaviour
+{
+    public GUIStyle windowStyle;
+    public GUIStyle buttonStyle;
+
+    void OnGUI()
+    {
+        GUI.skin.font = Resources.Load<Font>("Fonts/Petitinho");
+        GUI.skin.button = buttonStyle;
+        GUI.skin.window = windowStyle;
+    }
+}
