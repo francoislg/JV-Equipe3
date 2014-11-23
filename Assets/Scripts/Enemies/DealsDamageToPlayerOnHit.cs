9e8e864c1e04aff3ec76ce45ff1e486da8bc86c5
francoislg <francois_lg@hotmail.com>
2014-11-23 14:55:12 -0500

    Upgrade de DealsDamageToPlayerOnHit et du TerrainGenerator

--- Assets/Scripts/Enemies/DealsDamageToPlayerOnHit.cs
+++ Assets/Scripts/Enemies/DealsDamageToPlayerOnHit.cs
@@ -4,44 +4,26 @@ using System.Collections;
 public class DealsDamageToPlayerOnHit : MonoBehaviour
 {
     public int damageCount = 10;
-    public double attackAnnimationSpeed;
-
-    ZombiAnimator animator;
-    float attackStartTime;
+	private EnemyHasLife enemyObject;
+	private bool isActive = true;
 
     void Start()
     {
-        animator = (ZombiAnimator)GetComponent(typeof(ZombiAnimator));
+        enemyObject = GetComponent<EnemyHasLife>() as EnemyHasLife;
     }
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Player" &&
-            animator.state != ZombiAnimator.State.Dying)
+		if(enemyObject != null){
+			if(!enemyObject.isAlive()){
+				isActive = false;
+			}
+		}
+		if (isActive && other.gameObject.tag == "Player")
         {
-            animator.state = ZombiAnimator.State.Attacking;
-            attackStartTime = Time.time;
-        }
-    }
-
-    void OnCollisionStay(Collision other)
-    {
-        if (other.gameObject.tag == "Player" &&
-            animator.state != ZombiAnimator.State.Dying &&
-            Time.time - attackStartTime > attackAnnimationSpeed)
-        {
-            PlayerHasLife playerLife = other.gameObject.GetComponent<PlayerHasLife>() as PlayerHasLife;
-            playerLife.ReceiveDamage(damageCount);
-            playerLife.PushFromSource(transform.position, damageCount * 100);
-            attackStartTime = Time.time;
-        }
-    }
-
-    void OnCollisionExit(Collision other)
-    {
-        if (animator.state != ZombiAnimator.State.Dying)
-        {
-            animator.state = ZombiAnimator.State.Walking;
+			PlayerHasLife playerLife = other.gameObject.GetComponent<PlayerHasLife>() as PlayerHasLife;
+			playerLife.ReceiveDamage(damageCount);
+			playerLife.PushFromSource(transform.position, damageCount);
         }
     }
 }
