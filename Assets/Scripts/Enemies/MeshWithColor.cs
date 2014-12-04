09aa6f9921586c65599e920d2fcb96998115b6a5
francoislg <francois_lg@hotmail.com>
2014-12-03 19:36:55 -0500

    La porte est maintenant rose

---
+++ Assets/Scripts/Enemies/MeshWithColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshWithColor : MonoBehaviour {
+
+	public Color color;
+	// Use this for initialization
+	void Start () {
+		Mesh mesh = GetComponentInChildren<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Color[] colors = new Color[vertices.Length];
+		int i = 0;
+		while (i < vertices.Length) {
+			colors[i] = color;
+			i++;
+		}
+		mesh.colors = colors;
+	}
+}
