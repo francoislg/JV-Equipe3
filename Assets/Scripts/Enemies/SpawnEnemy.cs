e64c700b4d8cc6cf8c3f5ee33a1085a819265aec
francoislg <francois_lg@hotmail.com>
2014-12-14 15:04:34 -0500

    Ajouté un nombre initial de spawn pour les ennemis

--- Assets/Scripts/Enemies/SpawnEnemy.cs
+++ Assets/Scripts/Enemies/SpawnEnemy.cs
@@ -8,17 +8,38 @@ public class SpawnEnemy : MonoBehaviour
     protected float nextSpawn;
    	public float cooldown = 5f;
     public float range = 25;
+	public int initNumber = 1;
+
+	private bool activated = false;
+
+	void Start(){
+		for(int i=0; i<initNumber; i++){
+			Spawn();
+		}
+	}
 
     void Update()
     {
-        if (Time.time > nextSpawn)
-        {
-            nextSpawn = Time.time + cooldown;
-            Vector3 randomPos = Random.insideUnitCircle * range;
-            randomPos.z = randomPos.y;
-            randomPos.y = 0;
-
-            Instantiate(copyUnit, transform.position + randomPos, Quaternion.identity);
-        }
+		if(activated){
+			if (Time.time > nextSpawn)
+	        {
+	            nextSpawn = Time.time + cooldown;
+				Spawn();
+	        }
+		}else{
+			Vector3 positionInScreen = Camera.main.WorldToViewportPoint(transform.position);
+			if(positionInScreen.x >= 0 && positionInScreen.x <= 1 && positionInScreen.y >= 0 && positionInScreen.y <= 1){
+				activated = true;
+			};
+		}
     }
+
+	private void Spawn(){
+		Vector3 randomPos = Random.insideUnitCircle * range;
+		randomPos.z = randomPos.y;
+		randomPos.y = 0;
+		
+		Instantiate(copyUnit, transform.position + randomPos, Quaternion.identity);
+	}
+
 }
