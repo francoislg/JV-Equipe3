f21b1978b38cf300ec14a36c038b60fc44be481f
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2014-12-14 23:46:58 -0500

    Small code style enchancement

--- Assets/Scripts/ItemRotator.cs
+++ Assets/Scripts/ItemRotator.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ItemRotator : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(new Vector3(0, 1, 0), 15*Time.deltaTime, Space.World);
-	}
+public class ItemRotator : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), 15 * Time.deltaTime, Space.World);
+    }
 }
